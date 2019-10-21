a16fb1c4d9510f01936239b24bed0a1a5806e11a
Kyle Robertson <kyle.robertson@revature.net>
2019-10-21 14:25:01 -0400

    Lost everything, starting from scratch

---
+++ Project0/Project0/AccType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0
+{
+    public enum AccType
+    {
+        Checking,
+        Business,
+        Loan,
+        CD
+    }
+}
---
+++ Project0/Project0/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0
+{
+    abstract class Account
+    {
+        public int AccountNumber { get; set; }
+        public double Balance { get; set; }
+        public double InterestRate { get; set; }
+    }
+}
